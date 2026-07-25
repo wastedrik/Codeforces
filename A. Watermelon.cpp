158530e45ade742b48bb8a9a45fb981714675bc5
Gregorius Enrico Markandeya <gregoriusenrico24@gmail.com>
2026-07-26 05:44:19 +0700

    Add files via upload

---
+++ A. Watermelon.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int n = 0;
+  std::cin >> n;
+  
+  if (n % 2 == 0) {std::cout << "YES" << std::endl;}
+  else {std::cout << "NO" << std::endl;}
+  return 0;
+}
